123d383e5523fc219cb3fb7030d977ac90643d1c
delza <delza@livingcode.org>
2011-01-31 07:57:41 -0800

    remove unneeded code for preventing desktop dragging

--- test/dragging.js
+++ test/dragging.js
@@ -1,10 +1,3 @@
-// document.ontouchstart = null;
-// $('html').get(0).ontouchmove = function(event){
-//     console.log('Preventing the drag');
-//      event.preventDefault();
-// };
-
-
 $('.sprite').drag(function(evt, dd){
     console.log('draggin');
     $(this).css({left: dd.offsetX, top: dd.offsetY});

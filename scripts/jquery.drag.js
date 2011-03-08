3a1ad95694c3c731af84eb36d773b503bf65e2ed
delza <delza@livingcode.org>
2011-03-07 21:33:10 -0800

    building up drag'n'drop slowly

--- scripts/jquery.drag.js
+++ scripts/jquery.drag.js
@@ -34,16 +34,44 @@
 // Key to jquery.event.touch is the timer function for handling movement and hit testing
 
 (function($){
-    var drag_target = null;
-    var target_canvas = null;
-    var drop_targets = [];
-    var start_position = null;
+    var drag_target, target_canvas, drop_targets, start_position, timer, dragging;
     var is_touch = window.ontouchstart && true;
     var drag_timeout = 20;
+
+    function reset(){
+        drag_target = null;
+        target_canvas = null;
+        drop_targets = [];
+        start_position = null;
+        timer = null;
+        dragging = false;
+        on_drag = start_drag; // prime trampoline
+    }
     
-    function init_mouse_drag(event){
-        // Called on mousedown, we haven't started dragging yet
-        // TODO: if anything but mouse button 1, return
+    reset();
+    
+    function blend(event){
+        if (is_touch){
+            if (event.originalEvent.touches.length > 1){
+                return false;
+            }
+            var touch = event.originalEvent.touches[0];
+            event.target = touch.target;
+            event.pageX = touch.pageX;
+            event.pageY = touch.pageY;
+        }else{
+            if (event.which !== 1){ // left mouse button 
+                return false;
+            }
+        }
+        // fix target?
+        return event;
+    }
+        
+    function init_drag(event){
+        // Called on mousedown or touchstart, we haven't started dragging yet
+        // TODO: Don't start drag on a text input
+        if (!blend(event)) return;
         var target = $(event.target).closest('.wrapper');
         if (target.length){
             drag_target = target;
@@ -54,15 +82,50 @@
         }
     }
     
-    function start_mouse_drag(event){
+    function start_drag(event){
+        // called on mousemove or touchmove if not already dragging
+        if (!blend(event)) return;
         // if no target, return
         // target = clone target if in menu
         // get position and append target to .content, adjust offsets
         // set last offset
         // start timer for drag events
+        timer = setTimeout(hit_test, drag_timeout);
+        continue_drag(event);
     }
-        
-}
-
+    
+    function continue_drag(event){
+        if (!blend(event)) return;
+        // update the variables, distance, button pressed
+    }
+    
+    function end_drag(end){
+        clearTimeout(timer);
+        // TODO:
+           // 1. Drop if there is a target
+           // 2. Remove, if not over a canvas
+           // 3. Remove, if dragging a clone
+           // 4. Move back to start position if not a clone (maybe not?)
+        reset();
+    }
+    
+    function hit_test(){
+        // test the dragging rect(s) against the target rect(s)
+        // test all of the left borders first, then the top, right, bottom
+        // goal is to eliminate negatives as fast as possible
+        timer = setTimeout(hit_test, drag_timeout);
+    }
+    
+    if (is_touch){
+        $('.scripts_workspace, .block_menu')
+            .delegate('.block', 'mousedown', init_drag)
+            .delegate('.block', 'mousemove', on_drag)
+            .delegate('.block', 'mouseup', end_drag);
+    }else{
+        $('.scripts_workspace, .block_menu')
+            .delegate('.block', 'touchstart', init_drag)
+            .delegate('.block', 'touchmove', on_drag)
+            .delegate('.block', 'touchend', end_drag);
+    }
     
 })(jQuery);

a97d66f0b9b418472ebd417f06ff9818ff6d5471
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2012-06-26 08:08:28 +0300

    Code not part of the new pluging structure

--- plugins/canvas.js
+++ plugins/canvas.js
@@ -1364,8 +1364,6 @@ var demos = [
 populate_demos_dialog(demos);
 load_current_scripts();
 $('.scripts_workspace').trigger('init');
-$('#block_menu').accordion({ autoHeight: false });
-console.log("Done");
 
 $('.socket input').live('click',function(){
     $(this).focus();

c0d287bae1c9826dca10a740d9ecede575bb42b5
dethe <dethe@livingcode.org>
2012-08-03 16:18:08 -0700

    turn tabs back on, remove dead code

--- scripts/ui.js
+++ scripts/ui.js
@@ -2,20 +2,6 @@
 
 // UI Chrome Section
 
-// function accordion(event){
-//     // console.log('accordion');
-//     var self = $(this);
-//     if (self.hasClass('selected')){
-//         self.removeClass('selected').siblings('.option').slideUp('slow');
-//         return;
-//     }
-//     $('.select.selected').removeClass('selected').siblings('.option').slideUp('slow');
-//     self.addClass('selected').siblings('.option').slideDown('slow');
-//     $('#block_menu').trigger('open', self);
-// }
-//$('#block_menu').delegate('.select', 'click', accordion);
-
-
 function tabSelect(event){
     var self = $(this);
     $('.tab_bar .selected').removeClass('selected');

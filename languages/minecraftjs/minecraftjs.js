3bc2d4533b9b4a52d69206ea37ae54cb2a056d32
Martyn Eggleton <martyn.eggleton@gmail.com>
2013-07-31 13:37:40 +0100

    adds code to communticate with nodejs server

--- languages/minecraftjs/minecraftjs.js
+++ languages/minecraftjs/minecraftjs.js
@@ -1,3 +1,4 @@
+
 /*
  *    MINECRAFTJS PLUGIN
  *
@@ -43,21 +44,6 @@ jQuery.fn.extend({
 });
 
 // End UI section
-
-// expose these globally so the Block/Label methods can find them
-window.choiceLists = {
-    keys: 'abcdefghijklmnopqrstuvwxyz0123456789*+-./'
-        .split('').concat(['up', 'down', 'left', 'right',
-        'backspace', 'tab', 'return', 'shift', 'ctrl', 'alt',
-        'pause', 'capslock', 'esc', 'space', 'pageup', 'pagedown',
-        'end', 'home', 'insert', 'del', 'numlock', 'scroll', 'meta']),
-        blocks:["AIR","STONE","GRASS","DIRT","COBBLESTONE","WOOD_PLANKS","SAPLING","BEDROCK","WATER_FLOWING","WATER_STATIONARY","LAVA_FLOWING","LAVA_STATIONARY","SAND","GRAVEL","GOLD_ORE","IRON_ORE","COAL_ORE","WOOD","LEAVES","GLASS","LAPIS_LAZULI_ORE","LAPIS_LAZULI_BLOCK","SANDSTONE","BED","COBWEB","GRASS_TALL","WOOL","FLOWER_YELLOW","FLOWER_CYAN","MUSHROOM_BROWN","MUSHROOM_RED","GOLD_BLOCK","IRON_BLOCK","STONE_SLAB_DOUBLE","STONE_SLAB","BRICK_BLOCK","TNT","BOOKSHELF","MOSS_STONE","OBSIDIAN","TORCH","FIRE","STAIRS_WOOD","CHEST","DIAMOND_ORE","DIAMOND_BLOCK","CRAFTING_TABLE","FARMLAND","FURNACE_INACTIVE","FURNACE_ACTIVE","DOOR_WOOD","LADDER","STAIRS_COBBLESTONE","DOOR_IRON","REDSTONE_ORE","SNOW","ICE","SNOW_BLOCK","CACTUS","CLAY","SUGAR_CANE","FENCE","GLOWSTONE_BLOCK","BEDROCK_INVISIBLE","GLASS_PANE","MELON","FENCE_GATE","GLOWING_OBSIDIAN","NETHER_REACTOR_CORE"],
-        cameramode:['normal','thirdPerson','fixed'],
-        blocktypes: ['step', 'expression', 'context', 'eventhandler'],
-    types: ['string', 'number', 'boolean', 'array', 'object', 'function', 'any'],
-    rettypes: ['none', 'string', 'number', 'boolean', 'array', 'object', 'function', 'any']
-};
-
 */
 
 wb.wrap = function(script){
@@ -65,21 +51,71 @@ wb.wrap = function(script){
 }
 
 function runCurrentScripts(event){
-    if (document.body.className === 'result' && wb.script){
-        wb.runScript(wb.script);
-    }else{
         var blocks = wb.findAll(document.body, '.workspace .scripts_workspace');
-        document.body.className = 'result';
         wb.runScript( wb.prettyScript(blocks) );
-    }
+        
 }
 Event.on('.runScripts', 'click', null, runCurrentScripts);
 
+
+wb.ajax = {
+        jsonp: function(url, data, success, error){
+            var callbackname = '_callback' + Math.round(Math.random() * 10000);
+            window[callbackname] = function(result){
+                delete window[callbackname];
+                success(result);
+            };
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function(){
+                if (xhr.readyState === 4){
+                    if (xhr.status === 200){
+                        // this is particularly unsafe code
+                        eval(xhr.responseText); // this should call window[callbackname]
+                    }else{
+                        delete window[callbackname];
+                        error(xhr);
+                    }
+                }
+            };
+            xhr.open('GET', url + '?' + data + '&callback=' + callbackname, true);
+            xhr.send();
+        }
+    };
+
 wb.runScript = function(script){
-    wb.script = script;
-    var runtimeUrl = location.protocol + '//' + location.host + '/dist/javascript_runtime.min.js';
-    console.log('trying to load library %s', runtimeUrl);
-    document.querySelector('.stageframe').contentWindow.postMessage(JSON.stringify({command: 'loadlibrary', library: runtimeUrl, script: wb.wrap(script)}), '*');
+  
+    var params = {'code':script};
+  
+    var keys = Object.keys(params);
+    var parts = [];
+    keys.forEach(function(key){
+      if (Array.isArray(params[key])){
+        params[key].forEach(function(value){
+          parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
+        });
+      }else{
+        parts.push(encodeURIComponent(key) + '=' + encodeURIComponent(params[key]));
+      }
+    });
+    var query = parts.join('&');
+    
+    var messagebox = document.querySelector('#messagebox');
+    if(messagebox === null || messagebox.length === 0)
+    {
+        messagebox = wb.elem('div', {"id":"messagebox"});
+        document.querySelector('.tabbar').appendChild(messagebox);
+        messagebox = document.querySelector('#messagebox');
+        
+    }
+    messagebox.innerHTML = "Sending Code to Raspberry Pi";
+    
+    
+    
+    wb.ajax.jsonp("../run", query, function(){ messagebox.innerHTML = "Code running on RPi"; window.setTimeout(function(){messagebox.innerHTML="";}, 5000);}, function(){ messagebox.innerHTML = "Code failed / server not running on RPi"; window.setTimeout(function(){messagebox.innerHTML = "";}, 5000);});
+    
+    //var runtimeUrl = location.protocol + '//' + location.host + '/dist/javascript_runtime.min.js';
+    //console.log('trying to load library %s', runtimeUrl);
+    //document.querySelector('.stageframe').contentWindow.postMessage(JSON.stringify({command: 'loadlibrary', library: //runtimeUrl, script: wb.wrap(script)}), '*');
 }
 
 function clearStage(event){
@@ -94,7 +130,7 @@ wb.prettyScript = function(elements){
     var script = js_beautify(elements.map(function(elem){
             return wb.codeFromBlock(elem);
         }).join(''));
-    script = "var Minecraft = require('./minecraft-pi/lib/minecraft.js'); \n var client = new Minecraft('localhost', 4711, function() {\nvar zeros={x:0, y:0, z:0};\n"+script+"\n});";
+    script = "var Minecraft = require('./minecraft-pi/lib/minecraft.js'); \nvar client = new Minecraft('localhost', 4711, function() {\nvar zeros={x:0, y:0, z:0};\n"+script+"\n});";
     return script;
 };
 

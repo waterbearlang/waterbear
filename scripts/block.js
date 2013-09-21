3a54cf8ef47201119926b14fa130122d1b7db0e6
dethe <dethe@livingcode.org>
2013-09-21 16:01:00 -0400

    fix for colors not being quoted

--- scripts/block.js
+++ scripts/block.js
@@ -472,7 +472,7 @@
         }else{
             var value = wb.findChild(holder, 'input, select').value;
             var type = holder.parentElement.dataset.type;
-            if (type === 'string' || type === 'choice'){
+            if (type === 'string' || type === 'choice' || type === 'color'){
                 if (value[0] === '"'){value = value.slice(1);}
                 if (value[value.length-1] === '"'){value = value.slice(0,-1);}
                 value = value.replace(/"/g, '\\"');

290108638ddaece1f0fd316ee86aa7bfbf43151c
dethe <dethe@livingcode.org>
2013-12-28 01:09:19 -0800

    finally adding Team Water's motion blocks

---
+++ languages/javascript/motion_runtime.js
@@ -0,0 +1,49 @@
+(function(global){
+
+var accelerometer = {};
+accelerometer.direction = "";
+accelerometer._tasks = [];
+
+if(window.DeviceOrientationEvent) {
+    // always follow direction changes
+    window.addEventListener('deviceorientation', processData);
+} else {
+    console.warn("Detection of acceleration is not supported");
+}
+
+function processData(event) {
+
+    var left_right = event.gamma;
+    var front_back = event.beta;
+
+    var limit = 10;
+    accelerometer.direction = "";
+
+    if(left_right > limit && front_back > limit) {
+        accelerometer.direction = "upright";
+    } else if(left_right > limit && front_back < -limit) {
+        accelerometer.direction = "downright";
+    } else if(left_right < -limit && front_back < -limit) {
+        accelerometer.direction = "downleft";
+    } else if(left_right < -limit && front_back > limit) {
+        accelerometer.direction = "upleft";
+    } else if(front_back > limit) {
+        accelerometer.direction = "up";
+    } else if(left_right > limit) {
+        accelerometer.direction = "right";
+    } else if(front_back < -limit) {
+        accelerometer.direction = "down";
+    } else if(left_right < -limit) {
+        accelerometer.direction = "left";
+    }
+};
+
+$('body').bind("waterbear_close", function() {
+    $.each( accelerometer._tasks, function(i, j){
+        clearInterval( j );
+    } );
+});
+
+global.accelerometer = accelerometer;
+
+})(global);

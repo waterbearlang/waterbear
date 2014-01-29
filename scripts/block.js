a5b7b0b66aa01a3e07e887bc0934c4b72d4aa386
Edward Chiou <eddy3334@gmail.com>
2014-01-28 20:58:24 -0800

    fixed issue #491

--- scripts/block.js
+++ scripts/block.js
@@ -302,7 +302,7 @@
         if (!blockdesc.isTemplateBlock){
             var newBlock = null;
             if (desc.uValue){
-
+                //No block value
             } else if (desc.uBlock){
                 // console.log('trying to instantiate %o', desc.uBlock);
                 newBlock = Block(desc.uBlock);

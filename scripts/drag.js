45c7a919d493721238e1f42323ccaadb56596e2a
Wai Ho Leung <whleung@ucsd.edu>
2014-02-08 12:01:48 -0800

    Fixed issue #336; dragging prevents use of scroll wheel on mouse

--- scripts/drag.js
+++ scripts/drag.js
@@ -192,6 +192,7 @@
         // get position and append target to .content, adjust offsets
         // set last offset
         dragTarget.style.position = 'absolute'; // FIXME, this should be in CSS
+        dragTarget.style.pointerEvents = 'none'; // FIXME, this should be in CSS
         // WB-Specific
         document.querySelector('.content.editor').appendChild(dragTarget);
         // WB-Specific

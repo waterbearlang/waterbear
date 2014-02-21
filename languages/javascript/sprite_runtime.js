504d28dc49dea87ca2f0af5195331532e6747f54
Yelim Kim <yelim.kim@mail.utoronto.ca>
2014-02-21 07:08:05 -0500

    Update sprite_runtime.js

--- languages/javascript/sprite_runtime.js
+++ languages/javascript/sprite_runtime.js
@@ -102,10 +102,10 @@ Sprite.prototype.draw = function(ctx){
     //rotation
     if(this.image != null){
         ctx.save();
-		ctx.translate(this.getPos().x,this.getPos().y);
-		ctx.rotate( this.facingDegrees *Math.PI/180);
-		ctx.drawImage(this.image, 0, 0,this.size.w,this.size.h);
-		ctx.restore();
+	ctx.translate(this.getPos().x,this.getPos().y);
+	ctx.rotate( this.facingDegrees *Math.PI/180);
+	ctx.drawImage(this.image, 0, 0,this.size.w,this.size.h);
+	ctx.restore();
     }else{
         ctx.fillStyle = this.color;
         ctx.beginPath();
@@ -129,10 +129,10 @@ Sprite.prototype.draw = function(ctx){
         ctx.font = String(height) +"px Arial";
         ctx.fillStyle = this.tColor;
         ctx.save();
-		ctx.translate(this.getPos().x ,this.getPos().y );
-		ctx.rotate( this.facingDegrees *Math.PI/180);
+	ctx.translate(this.getPos().x ,this.getPos().y );
+	ctx.rotate( this.facingDegrees *Math.PI/180);
         ctx.fillText(this.text,this.size.w *0.5,this.size.h *0.6, this.size.w *0.8);
-		ctx.restore();
+	ctx.restore();
     }
 };
 

85a08ed24a7d2f49ebb27d80c8e6bb5782fbec95
Clyde Byrd III <byrdc@purdue.edu>
2014-03-07 11:14:30 -0500

    Setup mocha, chai, and sinon

---
+++ framework/specs/test.spec.js
@@ -0,0 +1,10 @@
+describe("A test suite", function() {
+  beforeEach(function() {
+  });
+  afterEach(function() {
+  });
+ 
+  it('should fail', function() {
+    expect(true).to.be.false;
+  });
+});

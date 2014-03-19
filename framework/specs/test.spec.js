5be1065cef69b8cae55720861b18641dee824cbd
Clyde Byrd III <byrdc@purdue.edu>
2014-03-19 12:03:58 -0500

    tests

--- framework/specs/test.spec.js
+++ framework/specs/test.spec.js
@@ -1,13 +1,13 @@
-describe("A test suite", function() {
+describe("javascript_runtime test suite", function() {
   beforeEach(function() {
-	 document.body.innerHTML = __html__['fixtures/javascript.html'];
+	 //document.body.innerHTML = __html__['fixtures/javascript.html'];
 	 //console.log(document.body.innerHTML);
   });
   afterEach(function() {
   });
  
   it('should fail', function() {
-    expect(true).to.be.false;
+    
 
 
   });

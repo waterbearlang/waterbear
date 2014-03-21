6ece6eb08077636572e66ad94fbc383e766a18b7
Dillon Matchett <dillon.matchett@gmail.com>
2014-03-21 13:54:18 -0700

    Added some other functionality to the scheme language file

--- languages/scheme/scheme.js
+++ languages/scheme/scheme.js
@@ -5,6 +5,7 @@
  */
 
 (function(wb,Event){
+    'use strict';
     var bscheme = new BiwaScheme.Interpreter(function(e, state) {
         console.error(e.message);
     });
@@ -19,6 +20,30 @@
         }
         run();
     }
-}
-            
+
+    wb.writeScript = function(elements, view){
+        var script = elements.map(function(elem){
+            return wb.codeFromBlock(elem);
+        }).join('');
+        view.innerHTML = '<pre class="language-scheme">' + script + '</pre>';
+    };
+
+    wb.runCurrentScripts = function(){ /* do nothing */ };
+        
+    wb.clearStage = function(){ /* do nothing */ };
+
+
+    // NOTE: Taken directly from the JS file needs modification
+    // expose these globally so the Block/Label methods can find them
+    wb.choiceLists = {
+        boolean: ['true', 'false'],
+        keys: 'abcdefghijklmnopqrstuvwxyz0123456789*+-./'
+            .split('').concat(['up', 'down', 'left', 'right',
+            'backspace', 'tab', 'return', 'shift', 'ctrl', 'alt',
+            'pause', 'capslock', 'esc', 'space', 'pageup', 'pagedown',
+            'end', 'home', 'insert', 'del', 'numlock', 'scroll', 'meta']),
+        blocktypes: ['step', 'expression', 'context', 'eventhandler', 'asset'],
+        types: ['string', 'number', 'boolean', 'array', 'object', 'function', 'any'],
+        rettypes: ['none', 'string', 'number', 'boolean', 'array', 'object', 'function', 'any']
+    }})(wb, Event);
 

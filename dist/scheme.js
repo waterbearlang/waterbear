1e42bd76bfa2a1d6e2957ca7a98b2d4b75f2acad
stephanos <sgt29@cornell.edu>
2014-04-04 14:35:20 -0400

    Added methods for getting various data from trees.

--- dist/scheme.js
+++ dist/scheme.js
@@ -6117,12 +6117,51 @@ wb.menu({
             "id": "7bc79894-a495-4464-ae5d-2d47b66cbfc6",
             "script": "(listToTree {{1}})",
             "type": "tree",
-            "help": "Create a binary tree of only one element",
+            "help": "Convert a list to a tree",
             "sockets": [
                 {
                     "name": "list",
                     "type": "list",
                     "suffix": "of tree"
+                }
+            ]
+        },
+        {
+            "blocktype": "expression",
+            "id": "03703fac-5252-456d-bb1b-46b9a1bd0ca8",
+            "script": "(car {{1}})",
+            "type": "tree",
+            "help": "get the left subtree. The tree cannot be a leaf",
+            "sockets": [
+                {
+                    "name": "left subtree of",
+                    "type": "tree"
+                }
+            ]
+        },
+        {
+            "blocktype": "expression",
+            "id": "53b0ff25-3761-4597-870e-76ffc4afeacf",
+            "script": "(caddr {{1}})",
+            "type": "tree",
+            "help": "get the right subtree. The free cannot be a leaf",
+            "sockets": [
+                {
+                    "name": "right subtree of",
+                    "type": "tree"
+                }
+            ]
+        },
+        {
+            "blocktype": "expression",
+            "id": "102ce9b4-ae29-4bd5-9bbc-effbbd3a177a",
+            "script": "(if (equals? (cdr {{1}}) ()) (car {{1}}) (cadr {{1}}))",
+            "type": "tree",
+            "help": "get the data of ",
+            "sockets": [
+                {
+                    "name": "right subtree of",
+                    "type": "tree"
                 }
             ]
         }

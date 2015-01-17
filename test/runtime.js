15a9fedb142c96aaa6719c73ad710a4ee32eb5c8
Eddie Antonio Santos <easantos@ualberta.ca>
2015-01-17 11:34:21 -0800

    Implement tests for rect.

--- test/runtime.js
+++ test/runtime.js
@@ -1,10 +1,12 @@
 /**
  * Tests for the underlying functions for blocks in runtime.js.
  *
- * Tests are named as {blockCategory}.{block} (as they are named in their
- * script attribute in their wb-expression/wb-control/wb-step).
+ * Tests are grouped using QUnit.module given their block category.
+ * Tests are named according to their script attribute in their
+ * wb-expression/wb-control/wb-step.
  *
- * util, and event MUST be included before this file.
+ * util.js, and event.js MUST be included before this file (so you can use
+ * anything in those files in your tests).
  */
 
 /* TODO: control */
@@ -23,26 +25,108 @@
 /* TODO: points */
 
 QUnit.module('rect');
-QUnit.test('rect.fromCoordinates', function (assert) {
+QUnit.test('fromCoordinates', function (assert) {
     var rect = runtime.rect;
 
     var r1 = rect.fromCoordinates(-1, -2, 1, 1);
 
     assert.ok(r1 instanceof util.Rect,
-              'The rect could not be created.');
+              'Creating a rect from coordinates');
     assert.throws(
         function () {
             rect.fromCoordinates(0, 0, -1, 1);
         },
-        'Created a rect with negative width'
+        'Creating a rect with negative width throws'
     );
     assert.throws(
         function () {
             rect.fromCoordinates(0, 0, 0, -1);
         },
-        'Created a rect with negative height'
+        'Creating a rect with negative height throws'
     );
 });
+
+QUnit.test('fromVectors', function (assert) {
+    var rect = runtime.rect;
+
+    var zero = new util.Vector(0, 0);
+    var identity = new util.Vector(1, 1);
+    var negVector = new util.Vector(-1, -1);
+
+    var r1 = rect.fromVectors(zero, identity);
+
+    assert.ok(r1 instanceof util.Rect,
+              'Creating a rect from vectors');
+    assert.throws(
+        function () {
+            rect.fromVectors(zero, negVector);
+        },
+        'Creating a rect with negative size throws'
+    );
+});
+
+QUnit.test('fromArray', function (assert) {
+    var rect = runtime.rect;
+
+    QUnit.ok(rect.fromArray([0, 0, 1, 1]) instanceof util.Rect,
+             'Creating a Rect from an array');
+    QUnit.throws(
+        function () { rect.fromArray([1,2,3]); },
+        'Creating from an array with less than four elements should throw'
+    );
+});
+
+
+QUnit.test('getPosition', function (assert) {
+    var rect = runtime.rect;
+    
+    var r1 = rect.fromCoordinates(37, 42, 1, 1);
+    var position = rect.getPosition(r1);
+    
+    assert.ok(position instanceof util.Vector,
+             'Position is returned as a Vector');
+    assert.strictEqual(position.x, 37);
+    assert.strictEqual(position.y, 42);
+});
+
+QUnit.test('getSize', function (assert) {
+    var rect = runtime.rect;
+    
+    var r1 = rect.fromCoordinates(0, 0, 536, 231);
+    var size = rect.getSize(r1);
+    
+    assert.ok(size instanceof util.Vector,
+             'Size is returned as a Vector');
+    assert.strictEqual(size.x, 536);
+    assert.strictEqual(size.y, 231);
+});
+
+QUnit.test('asArray', function (assert) {
+    var rect = runtime.rect;
+    
+    var r1 = rect.fromVectors(new util.Vector(0, 0), new util.Vector(1, 1));
+    
+    assert.deepEqual(rect.asArray(r1), [0, 0, 1, 1],
+             'Rect can be returned as an array');
+});
+
+// Four methods combined into one!
+QUnit.test('get{X,Y,Width,Height}', function (assert) {
+    var rect = runtime.rect;
+    
+    var r1 = rect.fromVectors(new util.Vector(37, 42),
+                              new util.Vector(536, 231));
+    
+    assert.strictEqual(rect.getX(r1), 37,
+             'getX works');
+    assert.strictEqual(rect.getY(r1), 42,
+             'getY works');
+    assert.strictEqual(rect.getWidth(r1), 536,
+             'getWidth works');
+    assert.strictEqual(rect.getHeight(r1), 231,
+             'getHeight works');
+});
+
 
 /* TODO: sensing */
 /* TODO: motion */

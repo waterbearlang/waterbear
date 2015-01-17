67929c8f68274f18ec3d737b94453865143b6058
Jarrett Spiker <jspiker@shaw.ca>
2015-01-17 16:17:31 -0700

    implemented the text buttons

--- js/runtime.js
+++ js/runtime.js
@@ -419,14 +419,19 @@
             setFont: function (size, fontStyle){
                 var sizeString = size[0] + size[1];
                 ctx.font = sizeString + " " + fontStyle;
+                console.log(size);
+                console.log(sizeString + " " + fontStyle);
             },
             textAlign: function (alignment){ctx.textAlign = alignment;},
             textBaseline: function (baseline){ctx.textBaseline = baseline;},
-            fillText: function (text, x, y){},
-            fillTextWidth: function (text, x, y, width){},
-            strokeText: function (text, x, y){},
-            strokeTextWidth: function (text, x, y, width){},
-            width: function (text){return text.offsetWidth();}
+            fillText: function (text, x, y){ctx.fillText(text, x, y)},
+            fillTextWidth: function (text, x, y, width){ctx.fillText(text, x, y, width)},
+            strokeText: function (text, x, y){ctx.strokeText(text, x, y)},
+            strokeTextWidth: function (text, x, y, width){ctx.strokeText(text, x, y, width)},
+            width: function (text){
+                var textMetric = ctx.measureText(text);
+                return textMetric.width;
+            }
         }
     };
 

fea417648e76a8ca73f48a0069a586c1d2f33c26
dethe <dethe@livingcode.org>
2015-09-19 22:40:37 -0700

    fixed effect playback, playing twice still needs (a lot of) digging

--- js/runtime.js
+++ js/runtime.js
@@ -777,6 +777,7 @@
                 return assets.sounds[url]; // already cached by sounds library
             },
             play: function(sound){
+                console.log('sound.play()');
                 sound.play();
             },
             setLoop: function(sound, flag){
@@ -806,7 +807,7 @@
             stopReverb: function(sound){
                 sound.reverb = false;
             },
-            effect: function(frequency, attack, decay, waveform, volume, balance, wait, pitchBend, reverseBend, random, dissonance, echoDelay, echoFeedback, echoFilter){
+            effect: function(frequency, attack, decay, wait, echoDelay, echoFeedback, echoFilter, waveform, volume, balance, pitchBend, reverseBend, random, dissonance){
                 return {
                     play: function(){
                         soundEffect(

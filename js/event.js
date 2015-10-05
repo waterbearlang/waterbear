cd0bd69a63fb6020f45f3ad39b12aae926ef2639
dethe <dethe@livingcode.org>
2015-10-04 17:46:53 -0700

    handle meta key in various browsers, fixes #1209

--- js/event.js
+++ js/event.js
@@ -380,9 +380,12 @@
 
     var specialKeys = {
         // taken from jQuery Hotkeys Plugin
+        // updated because browsers suck, meta returned for 4 different values
+        // see https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent/keyCode#Non-printable_keys_%28function_keys%29
         8: "backspace", 9: "tab", 13: "return", 16: "shift", 17: "ctrl", 18: "alt", 19: "pause",
         20: "capslock", 27: "esc", 32: "space", 33: "pageup", 34: "pagedown", 35: "end", 36: "home",
         37: "left", 38: "up", 39: "right", 40: "down", 45: "insert", 46: "del",
+        91: "meta", 92: "meta", 93: "meta", // left and right commands
         96: "0", 97: "1", 98: "2", 99: "3", 100: "4", 101: "5", 102: "6", 103: "7",
         104: "8", 105: "9", 106: "*", 107: "+", 109: "-", 110: ".", 111 : "/",
         112: "f1", 113: "f2", 114: "f3", 115: "f4", 116: "f5", 117: "f6", 118: "f7", 119: "f8",

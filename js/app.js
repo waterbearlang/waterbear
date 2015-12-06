75f7ad26509d373c17a118502a1ff883ab997c14
Adrienne Pind <adriennepind@gmail.com>
2015-12-05 18:21:00 -0600

    Add missing comma

--- js/app.js
+++ js/app.js
@@ -204,7 +204,7 @@ function handleExampleButton(evt){
                 handleExample('Simple Bounce', 'bounce'),
                 handleExample('Simple Move', 'simple_move'),
                 handleExample('Simple Pong', 'simple_pong'),
-                handleExample('Draw from Array','array_draw')
+                handleExample('Draw from Array','array_draw'),
                 handleExample('Follow pointer', 'follow_pointer')
             ]
         }

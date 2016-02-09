3f3b8b34338b46519d761707306e17a0bab96d54
dethe <dethe@livingcode.org>
2016-02-08 20:20:53 -0800

    simplify and support multi-change undo/redo steps

--- js/undo.js
+++ js/undo.js
@@ -7,113 +7,48 @@
     var undoStack = [];
     var redoStack = [];
 
+    function removeFromEvent(evt) {
+        evt.addedTo.removeChild(evt.addedBlock);
+    }
+
+    function insertFromEvent(evt) {
+        evt.insertBefore(evt.deletedBlock, evt.nexBlock);
+    }
+
+    var undoFns = {
+        'remove-block': insertFromEvent,
+        'add-block': removeFromEvent
+    }
+
+    var redoFns = {
+        'remove-block': removeFromEvent,
+        'add-block': insertFromEvent
+    }
+
     function undoEvent(toUndo) {
         if (undoStack.length !== 0) {
-            var toUndo = undoStack.pop();
-            redoStack.push(toUndo);
+            var undoEvts = undoStack.pop();
+            redoStack.push(undoEvts);
             setButtonStatus();
-            if (toUndo.type === 'delete-block') {
-                undoDelete(toUndo);
-            } else if (toUndo.type === 'add-block') {
-                undoAdd(toUndo);
-            } else {
-                undoMove(toUndo);
+            for (var i = undoEvts.length; i > 0; i--){ // count backwards when undo-ing
+                var evt = undoEvts[i-1];
+                undoFns[evt.type](evt);
             }
-        }
-    }
-
-    function undoAdd(toUndo) {
-        var addedBlock = toUndo.addedBlock;
-        var addedTo = toUndo.addedTo;
-        addedTo.removeChild(addedBlock);
-    }
-
-    function undoDelete(toUndo) {
-        var deletedBlock = toUndo.deletedBlock;
-        var deletedFrom = toUndo.deletedFrom;
-        var nextBlock = toUndo.nextBlock;
-        if (nextBlock) {
-            deletedFrom.insertBefore(deletedBlock, nextBlock);
-        } else {
-            deletedFrom.appendChild(deletedBlock);
-        }
-    }
-
-    function undoMove(toUndo) {
-        var addedBlock = toUndo.addedBlock;
-        var addedTo = toUndo.addedTo;
-        var origParent = toUndo.originalParent;
-        var origNextEl = toUndo.originalNextEl;
-        //remove from parent expression
-        addedTo.removeChild(addedBlock);
-
-        if (toUndo.type === 'add-var-block') {
-            addedBlock = toUndo.insideBlock;
-        }
-
-        //add back to original parent expression
-        if (origNextEl) {
-            origParent.insertBefore(addedBlock, origNextEl);
-        } else {
-            origParent.appendChild(addedBlock);
         }
     }
 
     function redoEvent() {
         if (redoStack.length !== 0) {
-            var toRedo = redoStack.pop();
-            undoStack.push(toRedo);
+            var redoEvts = redoStack.pop();
+            undoStack.push(redoEvts);
             setButtonStatus();
-            if (toRedo.type === 'delete-block') {
-                redoDelete(toRedo);
-            } else if (toRedo.type === 'add-block') {
-                redoAdd(toRedo);
-            } else {
-                redoMove(toRedo);
+            for (var i = 0; i < redoEvts.length; i++){
+                var evt = redoEvts[i-1];
+                redoFns[evt.type](evt);
             }
         }
     }
 
-    function redoAdd(toRedo) {
-        var addedBlock = toRedo.addedBlock;
-        var addedTo = toRedo.addedTo;
-        var nextBlock = toRedo.nextBlock;
-        if (nextBlock) {
-            addedTo.insertBefore(addedBlock, nextBlock);
-        } else {
-            addedTo.appendChild(addedBlock);
-        }
-    }
-
-    function redoDelete(toRedo) {
-        var deletedBlock = toRedo.deletedBlock;
-        var deletedFrom = toRedo.deletedFrom;
-        deletedFrom.removeChild(deletedBlock);
-    }
-
-    function redoMove(toRedo) {
-        var addedBlock = toRedo.addedBlock;
-        var addedTo = toRedo.addedTo;
-        var origParent = toRedo.originalParent;
-        var nextBlock = toRedo.nextBlock;
-
-        //remove the block from it's original parent again
-        if (toRedo.type === 'move-block') {
-            origParent.removeChild(addedBlock);
-        } else { //type is add-var-block; must add back into setVariable block too
-            origParent.removeChild(toRedo.insideBlock);
-            addedBlock
-                .querySelector('wb-value[type="any"]')
-                .appendChild(toRedo.insideBlock);
-        }
-
-        //add block back to where it was before the undo
-        if (nextBlock) {
-            addedTo.insertBefore(addedBlock, nextBlock);
-        } else {
-            addedTo.appendChild(addedBlock);
-        }
-    }
 
     function undoKeyCombo(evt) {
         if ((Event.keyForEvent(evt) == 'z' && Event.keys['ctrl']) ||
@@ -164,7 +99,7 @@
 
     //add a new event to the undo stack
     function addNewEvent(evt) {
-        redoStack = [];
+        redoStack = []; // clear redoStack when we add a new undo event
         undoStack.push(evt);
         setButtonStatus();
     }
